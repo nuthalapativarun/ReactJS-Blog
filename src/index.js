7b947230a648270195cafc32c67991e055bf8915
Varun Nuthalapati <varun.nuthalapati@trinet.com>
2017-11-12 00:11:11 -0800

    ReactJS Blog

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reducers from './reducers';
+import promise from 'redux-promise';
+
+import PostsIndex from './components/posts_index';
+import PostNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostNew} />
+          <Route path="/posts/:id" component={PostsShow} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
